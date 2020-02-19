176d7748698a0e2b069838992b1ec9c5c47ae8fe
stncrllr <rellorcinatas@gmail.com>
2020-02-19 12:35:58 +0200

    testimonials slider, li__item:hover style

---
+++ bs4/test-slider.js
@@ -0,0 +1,16 @@
+var test_slider = tns({
+	container: '.testimonials-slider',
+	controls: false,
+	center: 'center',
+	loop: true,
+	items: 1,
+	slideBy: 'page',
+	nav: true,
+	navContainer: ".navtabs",
+	navPosition: "bottom",
+	autoplay: false,
+	speed: 400,
+	autoplayButtonOutput: false,
+	mouseDrag: true,
+	lazyload: true,
+});
